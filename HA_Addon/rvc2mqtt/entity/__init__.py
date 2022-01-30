3a75ac290d189130cec6c34d3b18a5795fd4834c
ccirrinc <ccirrinc@hotmail.com>
2022-01-29 22:50:48 -0800

    moving files around (#65)

---
+++ HA_Addon/rvc2mqtt/entity/__init__.py
@@ -0,0 +1,89 @@
+"""
+
+EntityPluginBaseClass
+
+Copyright 2022 Sean Brogan
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+import logging
+import queue
+from rvc2mqtt.mqtt import MQTT_Support
+
+class EntityPluginBaseClass(object):
+    """ Baseclass for all device entities
+    
+    Make a subclass for a new object
+    and define 
+
+    """  
+    def __init__(self, data:dict, mqtt_support: MQTT_Support):
+
+        if not hasattr(self, "id"):
+            # this seems like a bad code pattern..but ok for now
+            raise Exception("self.id must be defined")
+        
+        self.Logger = logging.getLogger(__class__.__name__)
+        self.mqtt_support: MQTT_Support = mqtt_support
+
+        # Make the required one status/state topic
+        self.status_topic: str = mqtt_support.make_device_topic_string(self.id, None, True)
+
+    def process_rvc_msg(self, new_message: dict) -> bool:
+        """ Process an incoming rvc message and determine if it
+        is of interest to this instance of this object.
+        
+        If relevant - Process the message and return True
+        else - return False
+        """
+        raise NotImplementedError()
+
+    def initialize(self):
+        """ Optional function 
+        Will get called once when the object is loaded.  
+        RVC canbus tx queue is available
+        mqtt client is ready.  
+        
+        This can be a good place to request data
+        """
+        pass
+
+    ########
+    # HELPER FUNCTIONS 
+    # NOT EXPECTING TO NEED TO BE OVERRIDDEN
+    ########
+    def _is_entry_match(self, match_entries: dict, rvc_msg: dict) -> bool:
+        '''
+        Determine if a RVC message matches the map_entries.  
+        All fields in match_entries must match the same fields in rvc_msg.
+
+        ret True if match
+        ret False if no match
+        
+        '''
+        for k,v in match_entries.items():
+            if k not in rvc_msg:
+                return False
+            
+            if rvc_msg[k] != v:
+                return False
+        
+        return True
+
+    def set_rvc_send_queue(self, send_queue: queue):
+        """ Provide queue for sending RVC messages.  Queue requires 
+        items be formatted as python-can messages"""
+        self.send_queue: queue = send_queue
+
---
+++ HA_Addon/setup/readme.md
@@ -0,0 +1,28 @@
+# RVC2MQTT
+
+This is an experimental project to connect an RV can bus to mqtt with the intent
+to monitor and control features of your RV.
+
+## Current Status
+
+| Host Type | Toolchain  | Build Status | Test Status | Code Coverage |
+| :-------- | :--------  | :-----       | :----       | :---          |
+| Linux     | Python 3.9 | <todo>       | <todo>      | <todo>        |
+
+
+## License
+
+All content in this repository is licensed under [Apache 2 License](LICENSE).
+
+
+## Contribution Process
+
+This project welcomes all types of contributions. 
+- Bugs/Feature request - Open a github issue
+- Question - Ask it in the github discussion forum
+- Code change - Open an Issue describing your change and then submit a PR.
+  - See docs for more details
+
+## Documentation
+
+See the github repo [__docs__](docs/overview.md) folder
